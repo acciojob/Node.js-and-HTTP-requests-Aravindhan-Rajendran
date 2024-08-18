3ac8e03d9c62a8d9237677c336d2ce04920bb052
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-08-18 23:28:33 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
 const https = require('https');
 
-// TODO: Implement this function
+// URL to fetch data from
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+// Perform the GET request
+https.get(url, (res) => {
+  let data = '';
+
+  // Listen for data chunks
+  res.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // When the response is complete
+  res.on('end', () => {
+    try {
+      // Parse the JSON response
+      const todo = JSON.parse(data);
+
+      // Output the title of the first todo item
+      console.log(todo.title);
+    } catch (error) {
+      console.error('Error parsing JSON:', error.message);
+    }
+  });
+}).on('error', (err) => {
+  console.error('Request failed:', err.message);
+});
